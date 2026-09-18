b12473be2f04e88c2e1b655a46e66bc792139fea
High-Tech777 <jenis.happy020@outlook.com>
2026-09-17 23:16:04 -0700

    javascript complete

--- index.js
+++ index.js
@@ -18,17 +18,18 @@ const showToast = (
         ) toastType = "info";
 
         let box = document.createElement("div");
-        box.classListadd(
+        box.classList.add(
             "toast", `toast-${toastType}`);
-        box.innerHTML = `<div class = "toast-content-wrapper">
-                            <div class = "toast-icon">
+        box.innerHTML = ` <div class="toast-content-wrapper">
+                            <div class="toast-icon">
                             ${icon[toastType]}
+                            </div>
+                            <div class = "toast-message">${message}</div>
+                            <div class = "toast-progress"></div>
                         </div>
-                        <div class = "toast-message">${message}</div>
-                        <div class = "toast-progress"></div>
                         `;
         duration = duration || 5000;
-        box.querySelector("toast-progress").computedStyleMap.animationDuration = `${duration / 1000}s`;
+        box.querySelector(".toast-progress").style.animationDuration = `${duration / 1000}s`;
 
         let toastAlready = document.body.querySelector(".toast");
         if (toastAlready) {
@@ -36,27 +37,27 @@ const showToast = (
         }
         document.body.appendChild(box)
     };
-let submit = document.querySelector(".custom-toast .success-toast");
-let information = document.querySelector(".custom-toast .info-toast");
-let failed = document.querySelector(".custom-toast .danger-toast");
-let warn = document.querySelector(".custom-toast .warning-toast");
+let submit = document.querySelector(".custom-toast.success-toast");
+let information = document.querySelector(".custom-toast.info-toast");
+let failed = document.querySelector(".custom-toast.danger-toast");
+let warn = document.querySelector(".custom-toast.warning-toast");
 
-submit.addEventListener("click", e=>() {
+submit.addEventListener("click", (e)=> {
     e.preventDefault();
-    showToast("Article Submitted Successully", "success", 5000);
+    showToast("Article Submitted Successfully", "success", 5000);
 });
 
-information.addEventListener("click", e=>() {
+information.addEventListener("click", (e)=> {
     e.preventDefault();
     showToast("Do POTD and Earn Coins", "info", 5000);
 });
 
-failed.addEventListener("click", e=>() {
+failed.addEventListener("click", (e)=> {
     e.preventDefault();
     showToast("Failed unexpected error", "danger", 5000);
 });
 
-warn.addEventListener("click", e=>() {
+warn.addEventListener("click", (e)=> {
     e.preventDefault();
     showToast("!warning! server error", "warning", 5000);
 });
